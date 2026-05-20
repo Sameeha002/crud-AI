cae65da6079455f895df0703bb3b85590e58da8d
Sameeha002 <sameehaa460@gmail.com>
2026-05-20 18:09:35 +0500

    fix: use DATABASE_URL env variable

--- crudAI-backend/database.py
+++ crudAI-backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = 'postgresql://postgres:psql@localhost/database'
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:psql@localhost/database")
 
 engine = create_engine(
     DATABASE_URL
